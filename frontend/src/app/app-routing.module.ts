ca555e0e129fb21481dff911d124f0c2465c367f
semanur03 <Semanur.Uyar@Student.HTW-Berlin.de>
2023-03-24 19:18:36 +0100

    fehler behoben

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { TabelleComponent } from './tabelle/tabelle.component';
 import { CreateComponent } from './create/create.component';
 import { FormComponent } from './tabelle/form/form.component';
 
 const routes: Routes = [
-  {path: 'header', component: HeaderComponent},
   {path: 'footer', component: FooterComponent},
   {path: 'tabelle', component: TabelleComponent},
   {path: 'tabelle/:id', component: TabelleComponent},
